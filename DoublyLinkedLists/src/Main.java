149d1010b0ecd0e92e038c6239c2cb6a2ef25f05
larryvsdata <larryvsdata@gmail.com>
2019-04-24 10:57:28 -0700

    Doubly linked List

---
+++ DoublyLinkedLists/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee janeJones = new Employee("Jane","Jones",123);
+		Employee johnDoe = new Employee("John","Doe",4568);
+		Employee marySmith = new Employee("Mary","Smith",22);
+		Employee mikeWilson = new Employee("Mike","Wilson",3245);
+		
+		
+		
+		
+	}
+
+}
