ee4e75acd34f154f5651df18e07592f4358cd906
larryvsdata <larryvsdata@gmail.com>
2019-05-09 18:06:52 -0700

    BST Delete implemented

--- BinarySearchTrees/src/Tree.java
+++ BinarySearchTrees/src/Tree.java
@@ -18,6 +18,32 @@ public class Tree {
 		return null;
 	}
 	
+	public void delete(int value) {
+		root = delete(root,value);
+	}
+	
+	private TreeNode delete(TreeNode subtreeRoot,int value) {
+		if(subtreeRoot == null) {
+			return subtreeRoot;
+		}
+		if(value < subtreeRoot.getData()) {
+			subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(),value));
+		}
+		else if (value > subtreeRoot.getData()) {
+			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(),value));
+		} else {
+			if(subtreeRoot.getLeftChild() == null) {
+				return subtreeRoot.getRightChild();
+			} else if (subtreeRoot.getRightChild()==null) {
+				return subtreeRoot.getLeftChild();
+			}
+			
+			subtreeRoot.setData(subtreeRoot.getRightChild().min());
+			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(),subtreeRoot.getData()));
+		}
+		return subtreeRoot;
+	}
+	
 	public int min() {
 		if (root==null) {
 		return Integer.MIN_VALUE;
